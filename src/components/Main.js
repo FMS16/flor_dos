9669333d5cf06a12fd9d9bf8d792a47bd03edf51
FMS16 <101989417+FMS16@users.noreply.github.com>
2024-06-10 20:54:15 -0300

    Second commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CountdownTimer from './CountdownTimer'
 
 const Main = () => {
-    return (
-        <div>
-            <main className='main'>
-                <div className='pista' id='pista-uno'>
-                    <p>1#</p>
-                    <div className='pista-contenido'>
-                        <h1>¡Es algo que hiciste muchás veces en tu vida!</h1>
-                        <h2>Y te encanta hacerlo...</h2>
-                    </div>
 
+return (
+    <div>
+        <main className='main'>
+            <div className='pista' id='pista-uno'>
+                <p>1#</p>
+                <div className='pista-contenido'>
+                    <h1>¡Es algo que hiciste muchás veces en tu vida!</h1>
+                    <h2>Y te encanta hacerlo...</h2>
                 </div>
-                <div className='pista' id='pista-dos'>
-                    <p>2#</p>
-                    <div className='pista-contenido'>
-                        <CountdownTimer targetDay={2} />
-                    </div>
+
+            </div>
+            <div className='pista' id='pista-dos'>
+                <p>2#</p>
+                <div className='pista-contenido'>
+                    <h1>Quer&iacute;as hacerlo conmigo apenas se pudiese as&iacute; que...</h1>
+                    <h2>¡Reserv&eacute; para hacerlo juntos este viernes!</h2>
                 </div>
-                <div className='pista' id='pista-tres'>
-                    <p>3#</p>
-                    <div className='pista-contenido'>
-                        <CountdownTimer targetDay={3} />
-                    </div>
+            </div>
+            <div className='pista' id='pista-tres'>
+                <p>3#</p>
+                <div className='pista-contenido'>
+                    <CountdownTimer targetDay={3} />
                 </div>
-                <div className='pista' id='pista-cuatro'>
-                    <p>4#</p>
-                    <div className='pista-contenido'>
-                        <CountdownTimer targetDay={4} />
-                    </div>
+            </div>
+            <div className='pista' id='pista-cuatro'>
+                <p>4#</p>
+                <div className='pista-contenido'>
+                    <CountdownTimer targetDay={4} />
                 </div>
-                {/* <ul className='pistas-adicionales'>
+            </div>
+            {/* <ul className='pistas-adicionales'>
                     <li>Increiblemente</li>
                     <li>No puedo definir el</li>
                     <li>Tanto amor que te tengo</li>
@@ -45,9 +47,9 @@ const Main = () => {
                     <li>Transformas el motor en rebeld&iacute;a</li>
                     <li>Enamor&aacute;ndome tuyo cada d&iacute;a..</li>
                 </ul> */}
-            </main>
-        </div>
-    )
+        </main>
+    </div>
+)
 }
 
 export default Main
